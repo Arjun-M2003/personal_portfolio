0c15a612279e5bddffaf323b9ca61e9c40f124ae
Arjun-M2003 <112738672+Arjun-M2003@users.noreply.github.com>
2025-08-08 18:37:30 -0400

    resume page update

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Renderer2 } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
 
 @Component({
   selector: 'app-resume',
-  imports: [],
+  imports: [AccordionModule, CommonModule],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.css'
 })
 export class ResumeComponent {
-  constructor(private titleService: Title) {
+
+  isWorkExperienceOpen: boolean = false;
+
+  constructor(private titleService: Title, private renderer: Renderer2) {
     this.titleService.setTitle('My Portfolio - Resume');
+  }
+
+  downloadFile() {
+    const link = this.renderer.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', '../../assets/Arjun_Resume_pg.pdf');
+    link.setAttribute('download', 'Arjun_Resume_pg.pdf');
+    link.click();
+    link.remove();
   }
 }
